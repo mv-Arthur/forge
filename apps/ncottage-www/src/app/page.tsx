f5c1ba780340c3b0657fc85620dd381e52d0e00e
malakhovarthur <99404909+mv-Arthur@users.noreply.github.com>
2026-04-26 13:28:08 +0300

    feat: ViewRequestSection (front-page__green-row parity) (#62)
    Co-authored-by: Artur Malakh <arthyrio@yandex.ru>

--- apps/ncottage-www/src/app/page.tsx
+++ apps/ncottage-www/src/app/page.tsx
@@ -9,12 +9,14 @@ import { GallerySection } from "@/components/sections/GallerySection";
 import { ReviewsSection } from "@/components/sections/ReviewsSection";
 import { CtaSection } from "@/components/sections/CtaSection";
 import { ContactForm } from "@/components/sections/ContactForm";
+import { ViewRequestSection } from "@/components/sections/ViewRequestSection";
 import {
     ADVANTAGES_SECTION,
     CATEGORIES_SECTION,
     HERO,
     PROJECT_PICKER,
     QUIZ_SECTION,
+    VIEW_REQUEST_SECTION,
 } from "@/lib/constants";
 import { getCategories, getReviews, getGallery } from "@/lib/data";
 
@@ -63,6 +65,17 @@ export default function HomePage() {
                 text={ADVANTAGES_SECTION.text}
                 background={ADVANTAGES_SECTION.background}
                 items={ADVANTAGES_SECTION.items}
+            />
+            <ViewRequestSection
+                title={VIEW_REQUEST_SECTION.title}
+                nameLabel={VIEW_REQUEST_SECTION.nameLabel}
+                namePlaceholder={VIEW_REQUEST_SECTION.namePlaceholder}
+                phoneLabel={VIEW_REQUEST_SECTION.phoneLabel}
+                phonePlaceholder={VIEW_REQUEST_SECTION.phonePlaceholder}
+                submitLabel={VIEW_REQUEST_SECTION.submitLabel}
+                privacy={VIEW_REQUEST_SECTION.privacy}
+                successTitle={VIEW_REQUEST_SECTION.successTitle}
+                successText={VIEW_REQUEST_SECTION.successText}
             />
             <PopularProjects />
             <StagesSection />
